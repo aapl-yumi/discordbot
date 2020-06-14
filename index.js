95b0cdcb1806358bc30ccdd1fbc49c0d6b3c8ebe
Yumi <yumiapple@protonmail.com>
2020-06-14 21:11:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -35,26 +35,26 @@ client.once("ready", () => {
 
 client.on("message", (message) => {
   if (message.content === `${prefix}help`) {
-    message.channel.send(
+    return message.channel.send(
       `For help, please visit https://yumiizumi.com/discordbot.`
     );
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  }
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(" ");
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
 
-    if (message.content === `${prefix}ping`) {
-      message.channel.send("Pong.", command);
-    } else if (message.content === `${prefix}server`) {
-      message.channel.send(`This server's name is: ${message.guild.name}`);
-    } else if (message.content === `${prefix}user-info`) {
-      message.channel.send(
-        `Your username: ${message.author.tag}\nYour ID: ${message.author.id}`
-      );
-    } else if (message.content === `${prefix}hello`) {
-      message.channel.send(`ブンブンハロー ${message.author}`);
-    }
+  if (message.content === `${prefix}ping`) {
+    message.channel.send("Pong.", command);
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(`This server's name is: ${message.guild.name}`);
+  } else if (message.content === `${prefix}user-info`) {
+    message.channel.send(
+      `Your username: ${message.author.tag}\nYour ID: ${message.author.id}`
+    );
+  } else if (message.content === `${prefix}hello`) {
+    message.channel.send(`ブンブンハロー ${message.author}`);
   }
 });
 
