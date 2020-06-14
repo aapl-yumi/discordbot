c2cdca113ba7759ccf20f30fa73156297ca5a4d7
Yumi <yumiapple@protonmail.com>
2020-06-14 14:46:34 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const prefix = "y!";
+
+client.once("ready", () => {
+  client.user
+    .setPresence({ activity: { name: "Epic VALORANT" }, status: "online" })
+    .then(console.log)
+    .catch(console.error);
+  console.log("Ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (message.content === `${prefix}ping`) {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send("Pong.");
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(`This server's name is: ${message.guild.name}`);
+  } else if (message.content === `${prefix}user-info`) {
+    message.channel.send(
+      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
+    );
+  } else if (message.content === `${prefix}hello`) {
+    message.channel.send(`hello`);
+  }
+});
+
+client.login("Njg0MDcwODEwNDg2NzAyMTQx.XuD5JA.XLbKjgkljhDZsSu_pvC99Jb9miU");
