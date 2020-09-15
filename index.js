4f7508954309d98e6a328c7c393281a4e4030518
Yumi Izumi <me@yumiizumi.com>
2020-09-15 13:18:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ client.once("ready", () => {
 
 client.on("message", (message) => {
   // if (message.author)
-  if (message.content.search("sad")) {
-    return message.channel.send(`omg so sad alexa play despacito ${message.author}`);
-  }
+  // if (message.content.search("sad")) {
+  //   return message.channel.send(`omg so sad alexa play despacito ${message.author}`);
+  // }
   if (message.content === `${prefix}help`) {
     return message.channel.send(
       `For help, please visit https://yumiizumi.com/discordbot, and hope there is any help there.`
