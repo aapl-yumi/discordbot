9f07e0851fb82cfdf11699b19f2e774ab4f432cc
aapl-yumi <yumiapple@protonmail.com>
2020-09-17 02:08:50 +0900

    Updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.once("ready", () => {
     },
     status: "idle",
   });
-  client.channels.get("755832089764691979").send("Ready " + new Date());
+  client.channels.cache.get("755832089764691979").send("Ready " + new Date());
   Console.log("Ready");
 });
 
