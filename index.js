012fd9baa3b73b6bcdf2b112846dbf2ea0ed1bf4
aapl-yumi <yumiapple@protonmail.com>
2020-09-17 01:48:18 +0900

    Updated

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.once("ready", () => {
 client.on("message", (message) => {
   firebase
     .database()
-    .ref("servers/" + message.guildID + "/autoresponder")
+    .ref("servers/" + "abc" + "/autoresponder")
     .set({ abc: message.guildID });
   if (message.content === "testtest") {
     return message.channel.send(JSON.stringify(message));
