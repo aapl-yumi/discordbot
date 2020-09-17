34b5d81a9981288d61b828248da16c0df3657cda
Yumi Izumi <me@yumiizumi.com>
2020-09-17 09:34:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ client.once("ready", () => {
 client.on("message", (message) => {
   firebase
     .database()
-    .ref("servers/")
-    .child(message.guildID)
+    .ref("servers/" + message.guildID)
     .set({ abc: message.guildID });
   if (message.content === "testtest") {
     return message.channel.send(JSON.stringify(message));
