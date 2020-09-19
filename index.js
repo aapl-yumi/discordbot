411c7f47ef089a02eb03424b0e6c2738bf0afc20
aapl-yumi <yumiapple@protonmail.com>
2020-09-20 04:02:57 +0900

    Updated

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ client.on("message", (message) => {
     .child("autores")
     .orderByChild("mes")
     .on("value", function (data) {
-      return Console.log(data);
+      return Console.log(data.val());
     });
 
   if (message.content === "testtest") {
