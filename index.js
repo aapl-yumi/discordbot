9f04f04c996264e6af6640be23a8a8922666b9cf
aapl-yumi <yumiapple@protonmail.com>
2020-09-20 18:39:22 +0900

    Updated

--- index.js
+++ index.js
@@ -31,140 +31,146 @@ client.once("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.author.bot) return;
   if (!message.content) return;
+  if (message.author.bot) {
+    return;
+  } else {
+    lowerCaseMessageContent = message.content.toLowerCase();
 
-  lowerCaseMessageContent = message.content.toLowerCase();
-
-  guild = firebase
-    .database()
-    .ref()
-    .child("servers/" + message.guild.id);
-  guild
-    .child("channels/" + message.channel.id)
-    .set({ name: message.channel.name });
-
-  if (message.member.hasPermission("ADMINISTRATOR")) {
+    guild = firebase
+      .database()
+      .ref()
+      .child("servers/" + message.guild.id);
     guild
-      .child("autoresque/inque")
-      .once("value")
-      .then(function (snapshot) {
-        inque = snapshot.val();
-        if (
-          !inque.mes &&
-          message.channel.id == inque.channel &&
-          message.author.id == inque.author &&
-          message.content !== `${prefix}ar`
-        ) {
-          if (message.content === "CANCEL") {
+      .child("channels/" + message.channel.id)
+      .set({ name: message.channel.name });
+
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+      guild
+        .child("autoresque/inque")
+        .once("value")
+        .then(function (snapshot) {
+          inque = snapshot.val();
+          if (
+            !inque.mes &&
+            message.channel.id == inque.channel &&
+            message.author.id == inque.author &&
+            message.content !== `${prefix}ar`
+          ) {
+            if (message.content === "CANCEL") {
+              guild.child("autoresque/inque").remove();
+              return message.channel.send("Autoresponder setup was cancelled.");
+            }
+            guild.child("autoresque/inque").set({
+              author: inque.author,
+              time: inque.time,
+              channel: inque.channel,
+              mes: message.content,
+            });
+            return message.channel.send(
+              'What should the response be? If you want to cancel, type in "CANCEL".'
+            );
+          } else if (
+            inque.mes &&
+            message.channel.id == inque.channel &&
+            message.author.id == inque.author &&
+            message.content !== `${prefix}ar`
+          ) {
+            if (message.content === "CANCEL") {
+              guild.child("autoresque/inque").remove();
+              return message.channel.send("Autoresponder setup was cancelled.");
+            }
+            key = guild.child("autores/" + inque.mes).set(message.content);
             guild.child("autoresque/inque").remove();
-            return message.channel.send("Autoresponder setup was cancelled.");
+            return message.channel.send(
+              "Autoresonder has been set. When you type in \n```" +
+                inque.mes +
+                "```the response will be```" +
+                message.content +
+                "```"
+            );
           }
+        });
+    }
+
+    guild
+      .child("autores")
+      .orderByValue()
+      .on("value", function (data) {
+        data.forEach(function (data) {
+          if (lowerCaseMessageContent.search(data.key) >= 0) {
+            return message.channel.send(data.val());
+          }
+        });
+      });
+
+    if (message.content === "testtest") {
+      return message.channel.send(JSON.stringify(message));
+    }
+
+    if (message.content === `${prefix}help`) {
+      return message.channel.send(
+        `For help, please visit https://yumiizumi.com/discordbot, and hope there is any help there.`
+      );
+    }
+
+    if (!message.guild) {
+      return message.channel.send(`no.`);
+    } else {
+      if (!message.content.startsWith(prefix)) return;
+      if (
+        message.guild &&
+        !message.channel
+          .permissionsFor(message.guild.me)
+          .missing("SEND_MESSAGES")
+      )
+        return;
+
+      const args = message.content.slice(prefix.length).split(" ");
+      const command = args.shift().toLowerCase();
+
+      if (message.content === `${prefix}ping`) {
+        return message.channel.send(`Pong. ${client.ws.ping} ms`);
+      } else if (message.content === `${prefix}server`) {
+        return message.channel.send(
+          `This server's name is: ${message.guild.name}`
+        );
+      } else if (
+        message.content === (`${prefix}aboutme` || `${prefix}whoami`)
+      ) {
+        return message.channel.send(
+          `${message.author}\nYour username: ${message.author.tag}\nYour ID: ${message.author.id}`
+        );
+      } else if (message.content === `${prefix}hello`) {
+        return message.channel.send(`ブンブンハロー ${message.author}`);
+      } else if (message.content === `${prefix}no`) {
+        return message.channel.send(`Because no.`);
+      } else if (
+        message.content.startsWith(`${prefix}calc ` || `${prefix}calculate `) &&
+        args.length > 0
+      ) {
+        return message.channel.send(`That's too hard for me`);
+      } else if (message.content === `${prefix}umi`) {
+        return message.channel.send(
+          `Visit <https://yumiizumi.com> for more information on Yumi.`
+        );
+      } else if (message.content.startsWith(`${prefix}ar`)) {
+        if (args == "list") {
+          return message.channel.send(
+            "Check the list of autoresponses for this server at https://yumiizumi.com/discordbot/?s=" +
+              message.guild.id +
+              "."
+          );
+        } else if (message.member.hasPermission("ADMINISTRATOR")) {
           guild.child("autoresque/inque").set({
-            author: inque.author,
-            time: inque.time,
-            channel: inque.channel,
-            mes: message.content,
+            author: message.author.id,
+            time: Math.floor(Date.now() / 1000),
+            channel: message.channel.id,
           });
           return message.channel.send(
-            'What should the response be? If you want to cancel, type in "CANCEL".'
-          );
-        } else if (
-          inque.mes &&
-          message.channel.id == inque.channel &&
-          message.author.id == inque.author &&
-          message.content !== `${prefix}ar`
-        ) {
-          if (message.content === "CANCEL") {
-            guild.child("autoresque/inque").remove();
-            return message.channel.send("Autoresponder setup was cancelled.");
-          }
-          key = guild.child("autores/" + inque.mes).set(message.content);
-          guild.child("autoresque/inque").remove();
-          return message.channel.send(
-            "Autoresonder has been set. When you type in \n```" +
-              inque.mes +
-              "```the response will be```" +
-              message.content +
-              "```"
+            'What should the message that initializes an autoresponse? If you want to cancel, type in "CANCEL".'
           );
         }
-      });
-  }
-
-  guild
-    .child("autores")
-    .orderByValue()
-    .on("value", function (data) {
-      data.forEach(function (data) {
-        if (lowerCaseMessageContent.search(data.key) >= 0) {
-          return message.channel.send(data.val());
-        }
-      });
-    });
-
-  if (message.content === "testtest") {
-    return message.channel.send(JSON.stringify(message));
-  }
-
-  if (message.content === `${prefix}help`) {
-    return message.channel.send(
-      `For help, please visit https://yumiizumi.com/discordbot, and hope there is any help there.`
-    );
-  }
-
-  if (!message.guild) {
-    return message.channel.send(`no.`);
-  } else {
-    if (!message.content.startsWith(prefix)) return;
-    if (
-      message.guild &&
-      !message.channel.permissionsFor(message.guild.me).missing("SEND_MESSAGES")
-    )
-      return;
-
-    const args = message.content.slice(prefix.length).split(" ");
-    const command = args.shift().toLowerCase();
-
-    if (message.content === `${prefix}ping`) {
-      return message.channel.send(`Pong. ${client.ws.ping} ms`);
-    } else if (message.content === `${prefix}server`) {
-      return message.channel.send(
-        `This server's name is: ${message.guild.name}`
-      );
-    } else if (message.content === (`${prefix}aboutme` || `${prefix}whoami`)) {
-      return message.channel.send(
-        `${message.author}\nYour username: ${message.author.tag}\nYour ID: ${message.author.id}`
-      );
-    } else if (message.content === `${prefix}hello`) {
-      return message.channel.send(`ブンブンハロー ${message.author}`);
-    } else if (message.content === `${prefix}no`) {
-      return message.channel.send(`Because no.`);
-    } else if (
-      message.content.startsWith(`${prefix}calc ` || `${prefix}calculate `) &&
-      args.length > 0
-    ) {
-      return message.channel.send(`That's too hard for me`);
-    } else if (message.content === `${prefix}umi`) {
-      return message.channel.send(
-        `Visit <https://yumiizumi.com> for more information on Yumi.`
-      );
-    } else if (message.content.startsWith(`${prefix}ar`)) {
-      if (args == "list") {
-        return message.channel.send(
-          "Check the list of autoresponses for this server at https://yumiizumi.com/discordbot/?s=" +
-            message.guild.id +
-            "."
-        );
-      } else if (message.member.hasPermission("ADMINISTRATOR")) {
-        guild.child("autoresque/inque").set({
-          author: message.author.id,
-          time: Math.floor(Date.now() / 1000),
-          channel: message.channel.id,
-        });
-        return message.channel.send(
-          'What should the message that initializes an autoresponse? If you want to cancel, type in "CANCEL".'
-        );
       }
     }
   }
