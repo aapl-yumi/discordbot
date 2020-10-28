39fc5830d9d621c515399b0da848d92eed6343a1
Yumi Izumi <me@yumiizumi.com>
2020-10-29 08:56:32 +0900

    Updated

--- index.js
+++ index.js
@@ -20,22 +20,9 @@ const prefix = "y!";
 
 const Console = console;
 
-wordnikapi = process.env.wordnikApiKey;
+var fs = require("fs");
 
-var getJSON = function (url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.responseType = "json";
-  xhr.onload = function () {
-    var status = xhr.status;
-    if (status === 200) {
-      callback(null, xhr.response);
-    } else {
-      callback(status, xhr.response);
-    }
-  };
-  xhr.send();
-};
+wordnikapi = process.env.wordnikApiKey;
 
 function findDef(word) {
   let url =
@@ -43,10 +30,11 @@ function findDef(word) {
     word +
     `/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=` +
     wordnikapi;
-  getJSON(url, gotData);
-  function gotData(data) {
-    return data[0].text;
-  }
+  let data = JSON.parse(fs.readFileSync(url, "utf8"));
+  return (
+    data[0].text ||
+    "Cannot find definition. Visit https://www.wordnik.com/words"
+  );
 }
 
 client.once("ready", () => {
