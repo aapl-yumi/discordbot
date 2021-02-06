44ffe4a47b88250d4025b543384ebc9c8715fe81
aapl-yumi <yumiapple@protonmail.com>
2021-02-06 18:57:49 +0900

    updated

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ client.on("message", (message) => {
               });
             if (acc > 95 && wpm > oldwpm) {
               guild.child("typing/leaderboard/" + message.author.id).set({
-                user: message.author.toString(),
+                user: message.author.username,
                 wpm: Math.floor(numWord / timeTook),
                 accuracy: acc,
               });
