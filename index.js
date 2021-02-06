7f17d40265e655a20e41e64272334a804c8d3ee8
aapl-yumi <yumiapple@protonmail.com>
2021-02-06 18:12:18 +0900

    updated

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ client.on("message", (message) => {
               });
             if (acc > 95 && wpm > oldwpm) {
               guild.child("typing/leaderboard/" + message.author.id).set({
-                user: message.author.id,
+                user: message.author,
                 wpm: Math.floor(numWord / timeTook),
                 accuracy: acc,
               });
@@ -315,6 +315,8 @@ client.on("message", (message) => {
             snapshot.forEach((snap) => {
               console.log(snap.val());
               lb +=
+                snap.val().user +
+                ": " +
                 snap.val().wpm +
                 "wpm, accuracy: " +
                 snap.val().accuracy +
